7296012be83f26c2ec60eb254128726f9b78fe61
suudii <bhowmickshubhadeep@gmail.com>
2020-02-05 07:17:22 +0530

    ap actually

--- train.py
+++ train.py
@@ -16,21 +16,22 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision import transforms
 from torch.autograd import Variable
+from utils.utils import compute_ap
 import torch.optim as optim
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--epochs", type=int, default=500, help="number of epochs")
 parser.add_argument("--image_folder", type=str, default="Data/images", help="path to dataset")
-parser.add_argument("--batch_size", type=int, default=2, help="size of each image batch")
+parser.add_argument("--batch_size", type=int, default=25, help="size of each image batch")
 parser.add_argument("--model_config_path", type=str, default="config/yolov3.cfg", help="path to model config file")
 parser.add_argument("--data_config_path", type=str, default="config/coco.data", help="path to data config file")
 parser.add_argument("--weights_path", type=str, default="config/yolov3.weights", help="path to weights file")
 parser.add_argument("--class_path", type=str, default="config/coco.names", help="path to class label file")
-parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
+parser.add_argument("--conf_thres", type=float, default=0.75, help="object confidence threshold")
 parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression")
 parser.add_argument("--n_cpu", type=int, default=0, help="number of cpu threads to use during batch generation")
 parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
-parser.add_argument("--checkpoint_interval", type=int, default=1, help="interval between saving model weights")
+parser.add_argument("--checkpoint_interval", type=int, default=20, help="interval between saving model weights")
 parser.add_argument("--checkpoint_dir", type=str, default="checkpoints", help="directory where model checkpoints are saved")
 parser.add_argument("--use_cuda", type=bool, default=True, help="whether to use cuda if available")
 opt = parser.parse_args()
@@ -126,6 +127,8 @@ for epoch in range(opt.epochs):
 
     #validation loss
     with torch.no_grad():
+       v_recall = []
+       v_prec = []
        for batch_i, (_, imgs, targets) in enumerate(val_dataloader):
         imgs = Variable(imgs.type(Tensor))
         targets = Variable(targets.type(Tensor), requires_grad=False)
@@ -139,6 +142,8 @@ for epoch in range(opt.epochs):
         val_nCorrect += model.losses["nCorrect"]
         val_nGT += model.losses["nGT"]
         val_nProp += model.losses["nProp"]
+        v_recall.append(model.losses["recall"])
+        v_prec.append(model.losses["precision"])
 
         str = "[Validation Epoch %d/%d, Batch %d/%d] [Losses: x %f, y %f, w %f, h %f, conf %f, cls %f, total %f, recall: %.5f, precision: %.5f ,ncorrect: %.5f ,nprop: %.5f ,nGT: %.5f ]\n" % (
                 epoch+1,
@@ -183,13 +188,16 @@ for epoch in range(opt.epochs):
     val_precision = 0
     if val_nProp > 0:
         val_precision = float(val_nCorrect / val_nProp)
+    ap = compute_ap(v_recall, v_prec)
 
-    str = "validation [Epoch %d/%d] [recall: %.5f, precision: %.5f]\n" % (
+    str = "validation [Epoch %d/%d] [recall: %.5f, precision: %.5f, ap: %.5f]\n" % (
                 epoch+1,
                 opt.epochs,
                 val_recall,
                 val_precision,
+                ap,
     )
+    
     print(str)
     f= open("log.txt","a+")
     f.write(str)
